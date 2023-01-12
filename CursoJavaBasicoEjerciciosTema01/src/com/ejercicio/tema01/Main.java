92e2faf4fcf850acb3a48c7ffdde46ff88daf12d
LEONARDOCHEBES <69778796+LEONARDOCHEBES@users.noreply.github.com>
2023-01-12 05:59:29 -0300

    Java basico Ejercicio tema 01
    subido

---
+++ CursoJavaBasicoEjerciciosTema01/src/com/ejercicio/tema01/Main.java
@@ -0,0 +1,25 @@
+package com.ejercicio.tema01;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int variableEntera;
+        long variableLong;
+        double variableDoble;
+        boolean variableBoleana;
+        String variableCadena;
+
+        variableEntera=125;
+        variableBoleana=true;
+        variableDoble=2.50000;
+        variableLong=266666666;
+        variableCadena="Soy una cadena";
+
+        System.out.println(variableLong);
+        System.out.println(variableDoble);
+        System.out.println(variableCadena);
+        System.out.println(variableBoleana);
+        System.out.println(variableEntera);
+        
+    }
+}
