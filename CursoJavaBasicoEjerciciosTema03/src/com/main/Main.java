ca440b691ad934acebff10525a14a8931af3e01c
LEONARDOCHEBES <69778796+LEONARDOCHEBES@users.noreply.github.com>
2023-01-18 02:37:04 -0300

    ejercicio tema 3
    listoo

---
+++ CursoJavaBasicoEjerciciosTema03/src/com/main/Main.java
@@ -0,0 +1,17 @@
+package com.main;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String[] nombres = {"pepe", "julio", "carlos", "estufa"};
+
+        for (String nombre : nombres){
+            System.out.print(nombre + " ");
+        }
+
+
+
+
+        //System.out.println("\nHello world!");
+    }
+}
